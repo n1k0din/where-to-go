27f4f6dc1ba8dc7552b968da249af063e5456404
n1k0din <nik726@gmail.com>
2021-05-25 17:45:48 +0700

    Add photo model

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Place
+from .models import Place, Photo
 
 admin.site.register(Place)
+admin.site.register(Photo)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Place(models.Model):
@@ -11,3 +12,25 @@ class Place(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+
+class Photo(models.Model):
+    image = models.ImageField('Изображение', upload_to='images', null=True, blank=True)
+    sort_index = models.SmallIntegerField(
+        'Порядковый номер',
+        default=1,
+        validators=[
+            MaxValueValidator(200),
+            MinValueValidator(1),
+        ]
+    )
+
+    place = models.ForeignKey(
+        Place,
+        related_name='place_photos',
+        verbose_name='Место',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.sort_index} {self.place.title}'
