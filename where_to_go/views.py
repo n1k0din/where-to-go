aa08380ee1ddb44cfc5ba012652c799cc9373314
n1k0din <nik726@gmail.com>
2021-05-26 11:24:19 +0700

    Change test HttpResponse to full JsonResponse

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
-from places.models import Place
+from places.models import Place, Photo
 
 
 def index(request):
@@ -37,4 +37,15 @@ def index(request):
 def place_detail(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
 
-    return HttpResponse(place.title)
+    place_metadata = {
+        'title': place.title,
+        'imgs': [photo.image.url for photo in place.place_photos.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.lat,
+            'lon': place.lon,
+        }
+    }
+
+    return JsonResponse(place_metadata, json_dumps_params={'ensure_ascii': False, 'indent': 2})
