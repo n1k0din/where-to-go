b8d46915c05a275437855cf301f344ae12841c17
n1k0din <nik726@gmail.com>
2021-05-26 14:43:37 +0700

    Add readme; rm photo model from admin panel

--- places/admin.py
+++ places/admin.py
@@ -8,18 +8,19 @@ from .models import Place, Photo
 class PhotoInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Photo
 
-    readonly_fields = ('image_preview', )
-    fields = ('image', 'image_preview', 'sort_index')
+    readonly_fields = ['image_preview']
+    fields = ['image', 'image_preview', 'sort_index']
 
     def image_preview(self, obj, width=200):
         return format_html('<img src={} width={}/>', obj.image.url, width)
 
 
 class PlaceAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
     inlines = [
         PhotoInline,
     ]
 
 
 admin.site.register(Place, PlaceAdmin)
-admin.site.register(Photo)
