219370bd9f31145d6698c55f24d592a149821fad
n1k0din <nik726@gmail.com>
2021-05-26 13:23:24 +0700

    Add photo preview in place edit page

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, Photo
 
 
 class PhotoInline(admin.TabularInline):
     model = Photo
+
+    readonly_fields = ('image_preview', )
+    fields = ('image', 'image_preview', 'sort_index')
+
+    def image_preview(self, obj, width=200):
+        return format_html('<img src={} width={}/>', obj.image.url, width)
 
 
 class PlaceAdmin(admin.ModelAdmin):
