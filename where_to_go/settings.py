867c7f646de7a02482081e2fe18a117c9a113229
n1k0din <nik726@gmail.com>
2021-06-01 11:27:36 +0700

    Fix static in template and settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -126,12 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
+STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
-    ]
-else:
-    STATIC_ROOT = env('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+]
+
+STATIC_ROOT = env('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
